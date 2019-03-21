1ad9956f1a47986f9b47b3003ff2c7ea2a986cc2
nahusznaj <nsznaj@gmail.com>
2019-03-21 09:02:43 +0000

    edits

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+#app instance of Flask
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
+#decorator -- instructs flask to run the view function and uses the return to respond to the client when client hits the endpoint
+@app.route('/hello-endpoint')
+def view_function():
     return 'Hello, World!'
 
 if __name__ == "__main__":
