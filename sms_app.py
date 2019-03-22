89ca07fd1d80a0935218b3e7355d33c9b9c2c6ee
nahusznaj <nsznaj@gmail.com>
2019-03-22 10:38:26 +0000

    edits

---
+++ sms_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, redirect
+from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+import requests 
+import json
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    resp = MessagingResponse() #set up the twiml response
+
+    ## quote API request with Requests. I used a POST, this is explained in their website.
+    r = requests.post('http://api.forismatic.com/api/1.0/', data = {'method':'getQuote', 'key':'457653', 'format':'json', 'lang':'en'})
+    quote_response = json.loads(r.text)
+    quote = quote_response['quoteText'] + ' ' + quote_response['quoteAuthor']
+    
+    # with twilio, read the body of the incoming message in the body variable 
+    body = request.values.get('Body', None) 
+
+    if body in ['Yes', 'Yes ', 'yEs', 'yEs ', 'yeS', 'yeS ', 'yes', 'yes ', 'YES',  'YES ']:
+        msg = resp.message(quote)
+        return str(resp)
+    elif body in ["No", "No ", "NO", "NO ", "nO", "nO "]:
+        resp.message("Oh... you said No!")
+        return str(resp)
+    else: 
+        resp.message("Please say Yes or No")
+        return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
